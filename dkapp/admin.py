b46de648c0902a8138b050f87f8d16817877942f
jackbover1 <teacher.teaching.angizeh@gmail.com>
2017-08-30 14:27:48 +0430

    sakhte modele dakhl

--- dkapp/admin.py
+++ dkapp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Token
+from .models import Token,Dakhl
 # Register your models here.
 admin.site.register(Token)
+admin.site.register(Dakhl)
--- dkapp/models.py
+++ dkapp/models.py
@@ -4,6 +4,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Token(models.Model):
     token=models.CharField(max_length=50)
-    nam=models.ForeignKey(User,on_delete=models.CASCADE)
+    nam=models.OneToOneField(User,on_delete=models.CASCADE)
     def __str__(self):
         return "{}".format(self.nam)
+    
+class Dakhl(models.Model):
+    tozih=models.CharField(max_length=100)
+    tarikh=models.DateTimeField()
+    mablagh=models.BigIntegerField()
+    nam=models.ForeignKey(User)
+    def __str__(self):
+        return "{}".format(self.mablagh)
