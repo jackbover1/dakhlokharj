9523b6c7125290beddf356cb8027cd6ee333e6e9
ANGIZEH <faryan.angizeh@gmail.com>
2017-08-31 13:12:02 +0430

    ezafe kardane pyc dar gitignor

--- dkapp/views.py
+++ dkapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.conf.urls import url
+from json import JSONEncoder
+from datetime import *
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from django.utils import timezone
 
 # Create your views here.
+
+def submit_expense(request):
+    this-token=request.POST['token']
